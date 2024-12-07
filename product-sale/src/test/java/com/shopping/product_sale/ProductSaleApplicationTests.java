9c42e10070a40b8004e1fe686bd494a55cd30f03
gulshensettarova <gulshensettarova@gmail.com>
2024-12-07 23:19:55 +0400

    Initial project

---
+++ product-sale/src/test/java/com/shopping/product_sale/ProductSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.product_sale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
