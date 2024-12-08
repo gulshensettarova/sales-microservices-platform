a34de221fda9855969f748c635569c8a7cab6b36
gulshensettarova <gulshensettarova@gmail.com>
2024-12-08 18:39:27 +0400

    Added database tables and inserted data

---
+++ product-sale/src/main/resources/db/changelog/1.0/1.0.1_create_tables.sql
@@ -0,0 +1,9 @@
+create table customer
+(
+    id       serial primary key,
+    name     varchar(30),
+    surname  varchar(30),
+    f_name   varchar(30),
+    email    varchar(30),
+    password varchar(30)
+)
---
+++ product-sale/src/main/resources/db/changelog/1.0/1.0.2_insert_data_into_table.sql
@@ -0,0 +1,3 @@
+insert into customer values
+        (1,'Gulshan','Sattarova','Elshan','gulshansattarova@gmail.com','123'),
+        (2,'Mahammad','Niyazli','Karim','mahammad.niyazli@gmail.com','123')
